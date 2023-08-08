a837b6d5bf6870711198b25fc6fd21440e93cbad
eran vinshelbaum <77538194+eranvin@users.noreply.github.com>
2023-08-08 20:36:05 +0300

    Create Valid_Parentheses.cpp

---
+++ Easy/Valid_Parentheses.cpp
@@ -0,0 +1,33 @@
+lass Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        char temp;
+
+        for(int i =0; i< s.length(); i++){
+            //check if open
+            if(s[i] == '(' | s[i] == '{' | s[i] == '['  )
+                stack.push(s[i]);
+            else{
+                if (stack.empty())
+                    return false;
+                temp = stack.top();
+                if(temp == '(')
+                    temp = ')';
+                else if(temp == '[')
+                    temp = ']';
+                else temp = '}';
+
+                if(s[i] != temp)
+                    return false;
+                else
+                    stack.pop();
+            }
+
+        }
+
+        
+
+        return stack.empty();
+    }
+};
